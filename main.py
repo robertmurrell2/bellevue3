e4417e2fe06f21cb481081e40ce157122214ba56
robertmurrell2 <26967105-robertmurrell2@users.noreply.replit.com>
2023-09-29 00:41:38 +0000

    changed from miles driven to feet walked

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 def feet_to_inches(feet):
-    return feet * 3
+  return feet * 3
+
 
 def main():
-    feet = float(input("Enter the number of miles driven: "))
-    inches = feet_to_inches(feet)
-    
-    print(f"Total miles driven: {miles} miles")
-    print(f"Total kilometers driven: {kilometers:.2f} kilometers")
+  feet = float(input("Enter the number of feet walked: "))
+  inches = feet_to_inches(feet)
+
+  print(f"Total feet walked: {feet} inches")
+  print(f"Total inches walked: {inches:.2f} inches")
+
 
 if __name__ == "__main__":
-    main()
+  main()
